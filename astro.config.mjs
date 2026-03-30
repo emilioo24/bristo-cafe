84564eb578b738282f23426479583d88008bfaff
emilianocalderon-vt <emiliano.calderon@vulnertrack.com>
2026-03-29 21:19:40 -0300

    Landing page Bristo Café 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://emilioo24.github.io',
+  base: '/bristo-cafe', // nombre exacto del repositorio en GitHub
+});
